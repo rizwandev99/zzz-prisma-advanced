5687b51c239e6802e084355e7fcd0f8351f9f2bf
Rizwan <rizwandev99@gmail.com>
2024-08-01 17:54:27 +0530

    completed prisma user

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,18 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function insertUser(
-  username: string,
-  password: string,
-  firstname: string,
-  lastname: string,
-  email: string
-) {
-  const response = await prisma.user.create({
-    data: {
-      username,
-      password,
-      firstname,
-      lastname,
-      email,
+async function getTodos(userId: number) {
+  const response = await prisma.todo.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      title: true,
+      description: true,
+      user: true,
     },
   });
   console.log(response);
 }
 
-insertUser("6", "7", "7", "7", "7");
+getTodos(5);
